8a252718a657b48d74847900d796d299f2c51edf
bagaswihant <bagaswihant@gmail.com>
2024-08-12 22:04:57 +0700

    fix fetch api and next image

--- app/page.js
+++ app/page.js
@@ -2,26 +2,23 @@ import { About } from "@/components/about";
 import Contact from "@/components/contact";
 import { HeroSection } from "@/components/hero-sections";
 import Project from "@/components/project";
+import next from "next";
 
 HeroSection
 const getapi = async () => {
-  try {
-    
-    return await res.json();
-    // return data.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const res = await fetch("https://admin-portfolio.wihant.com/api/getDataPortfolio",{next: {revalidate: 10}});
+    if(!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
 };
 export default async function Home() {
-  const res = await fetch("https://admin-portfolio.wihant.com/api/getDataPortfolio");
-  const data = await res.json();
-  // console.log(data.data.text[0])
+  const data = await getapi();
   return (
     <main className="bg-custom-100 overflow-hidden flex items-center justify-between mx-auto sm:px-6 flex-col">
-     <HeroSection text={data.data?.text[0]} />  
-     <About text={data.data?.text[0]} />
-     <Project project={data.data?.project} />
+     <HeroSection text={data?.text[0]} />  
+     <About text={data?.text[0]} />
+     <Project project={data?.project} />
      <Contact  />
     </main>
   );
